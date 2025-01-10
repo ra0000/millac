a1e2993e14f7427025c2f87fbc07c0e7cbfee978
ra0000 <61489224+ra0000@users.noreply.github.com>
2025-01-11 07:32:16 +0900

    ㅜㅜ

---
+++ js/index.js
@@ -0,0 +1,33 @@
+const flkty = new Flickity(".main-carousel", {
+    wrapAround: true,
+    autoPlay: 1400,
+    pauseAutoPlayOnHover: false,
+    prevNextButtons: false,
+    pageDots: false,
+    lazyLoad: 2,
+});
+
+const uap = new UAParser();
+let ua = uap.getResult();
+
+const call = (phone) => {
+    if (ua.device.type !== "mobile") {
+        alert("휴대폰에서만 바로 전화통화가 가능합니다. 전화번호: " + phone);
+        return;
+    }
+
+    location.href = "tel:" + phone;
+};
+
+const sms = (phone) => {
+    if (ua.device.type !== "mobile") {
+        alert("휴대폰에서만 바로 문자전송이 가능합니다. 전화번호: " + phone);
+        return;
+    }
+
+    if (ua.device.vendor === "Apple") {
+        location.href = "sms:" + phone + "&body=" + content;
+    } else {
+        location.href = "sms:" + phone + "?body=" + content;
+    }
+};
